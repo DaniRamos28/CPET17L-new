9b2f88761b7759aa393fb28e06babf3669a29283
nefetina <nefetinaira.tomas@gsfe.tupcavite.edu.ph>
2022-11-20 17:59:46 +0800

    Add Files

---
+++ techpeople/techpeople/pages/tp.js
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Learn from "../components/Learn";
+
+
+const TP = () => { 
+    return(
+        <div>
+            <Navbar />
+            <Hero />
+            <Learn />
+        </div>);
+};
+
+export default TP;
